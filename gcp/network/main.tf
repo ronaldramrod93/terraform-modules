a427f9c7cf9eb6ee8ed1f165f2c57dff5d035376
Ronald Ramos <143297851+ronaldramrod93@users.noreply.github.com>
2025-02-09 22:08:10 -0500

    added best practice naming conventions

--- gcp/network/main.tf
+++ gcp/network/main.tf
@@ -1,6 +1,8 @@
 resource "google_compute_network" "compute_network" {
   
+  # Best-practice: https://cloud.google.com/architecture/best-practices-vpc-design?hl=en#naming
   name                    = var.google_compute_network_name
+  
   description             = "${var.google_compute_network_description} (Created by terraform)"
   auto_create_subnetworks = var.google_compute_network_auto_create_subnetworks
   routing_mode            = var.google_compute_network_routing_mode
