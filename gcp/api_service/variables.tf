402a8712e44ac73d36624a0b22cb23dcf0233186
Ronald Ramos <ronald.ramrod.93@gmail.com>
2025-07-06 16:24:50 -0500

    add disable_dependent_services

--- gcp/api_service/variables.tf
+++ gcp/api_service/variables.tf
@@ -13,3 +13,9 @@ variable "disable_on_destroy" {
   description = "Disable the API services on destroy instead of let them enabled."
   default     = true
 }
+
+variable "disable_dependent_services" {
+  type        = bool
+  description = "Disable dependent services when disabling the API services."
+  default     = true
+}
